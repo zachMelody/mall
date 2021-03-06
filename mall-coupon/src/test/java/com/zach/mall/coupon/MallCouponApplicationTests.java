4bdf17a877fb5de84d1e3431e8ac0f6886bea505
zachMelody <zhoulv2012@hotmail.com>
2021-03-06 14:56:58 +0800

    init

---
+++ mall-coupon/src/test/java/com/zach/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zach.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mall-order/src/test/java/com/zach/mall/order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zach.mall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
